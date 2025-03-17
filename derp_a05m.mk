9721369fc0b7eb8e24602e0ac2972f194bce826c
Samsung Galaxy JDM-MTK <sathnidu.2022597@ninaro.lk>
2025-03-17 17:11:08 +0530

    Update and rename alpha_a05m.mk to derp_a05m.mk

--- derp_a05m.mk
+++ derp_a05m.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/alpha/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit from a05m device
 $(call inherit-product, device/samsung/a05m/device.mk)
@@ -20,13 +20,12 @@ $(call inherit-product, vendor/samsung/wing-camera/wingcamera-samsung.mk)
 TARGET_EXCLUDES_AUDIOFX := true
 
 PRODUCT_DEVICE := a05m
-PRODUCT_NAME := aosp_a05m
+PRODUCT_NAME := derp_a05m
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := Galaxy A05
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
-ALPHA_MAINTAINER := Smiley_9000
 WITH_GMS := true
 TARGET_BUILD_PACKAGE := 3
 
