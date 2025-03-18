48846cde91f251688d5321cfaeaa48257f040ba8
Samsung Galaxy JDM-MTK <sathnidu.2022597@ninaro.lk>
2025-03-18 07:58:06 +0530

    Update and rename derp_a05m.mk to lineage_a05m.mk

--- lineage_a05m.mk
+++ lineage_a05m.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from a05m device
 $(call inherit-product, device/samsung/a05m/device.mk)
@@ -20,7 +20,7 @@ $(call inherit-product, vendor/samsung/wing-camera/wingcamera-samsung.mk)
 TARGET_EXCLUDES_AUDIOFX := true
 
 PRODUCT_DEVICE := a05m
-PRODUCT_NAME := derp_a05m
+PRODUCT_NAME := lineage_a05m
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := Galaxy A05
 PRODUCT_MANUFACTURER := samsung
@@ -31,6 +31,19 @@ TARGET_BUILD_PACKAGE := 3
 
 WITH_GMS := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
+
+
+RISING_MAINTAINER="Smiley_9000"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="MediaTek G85" \
+    RisingMaintainer="Smiley_9000"
+
+RISING_MAINTAINER := Smiley_9000
+TARGET_ENABLE_BLUR := false
+
+TARGET_CORE_GMS := true
+TARGET_INCLUDE_GOOGLE_DIALER := true
+
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="a05mxx-user 14 UP1A.231005.007 A055FXXS7CXJ1 release-keys" \
